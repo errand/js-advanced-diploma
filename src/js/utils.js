639304c9b95c53473ce26d809052d1c3d5e04ead
Aleksandr Shatskikh <work@errand.ru>
2021-11-28 11:52:07 +0300

    feat: utils.js - finished and tested

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,11 +1,26 @@
 export function calcTileType(index, boardSize) {
+  if (index > 0 && index < (boardSize - 1)) {
+    return 'top';
+  }
+  if (index > (boardSize * boardSize - boardSize) && index < (boardSize * boardSize - 1)) {
+    return 'bottom';
+  }
+  if (index === (boardSize * boardSize - boardSize)) {
+    return 'bottom-left';
+  }
+  if (index === (boardSize * boardSize - 1)) {
+    return 'bottom-right';
+  }
   if (index === 0) {
     return 'top-left';
   }
   if (index === boardSize - 1) {
     return 'top-right';
   }
-  if (index === 15 || index === 23) {
+  if (index % boardSize === 0) {
+    return 'left';
+  }
+  if ((index + 1) % boardSize === 0) {
     return 'right';
   }
 
