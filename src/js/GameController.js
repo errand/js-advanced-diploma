b503aef05bc7c289b421c77b59858d79e0122acf
Aleksandr Shatskih <work@errand.ru>
2021-12-16 23:55:22 +0300

    wtf: fuck

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -13,8 +13,6 @@ export default class GameController {
     this.stateService = stateService;
     this.userTypes = [Bowman, Swordsman, Magician];
     this.aiTypes = [Vampire, Undead, Daemon];
-    this.userTeamPositions = new Set();
-    this.aiTeamPositions = new Set();
     this.currentLevel = 1;
     this.turn = 'user';
   }
@@ -23,19 +21,9 @@ export default class GameController {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
     this.gamePlay.drawUi('prairie');
-    this.userTeam = generateTeam([Bowman, Swordsman], 1, 2);
-    this.aiTeam = generateTeam(this.aiTypes, 1, 2);
-    this.userTeam.forEach(member => {
-      this.userTeamPositions.add(new PositionedCharacter(member, characterPosition().next().value));
-    });
-    this.aiTeam.forEach(member => {
-      this.aiTeamPositions.add(new PositionedCharacter(member, characterPosition('enemy').next().value));
-    });
-    this.gamePlay.redrawPositions([...this.userTeamPositions, ...this.aiTeamPositions]);
-
-    console.log(this.userTeamPositions);
 
     this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
+    this.gamePlay.addNewGameListener(this.onNewGameClick.bind(this));
   }
 
   onCellClick(index) {
@@ -48,5 +36,25 @@ export default class GameController {
 
   onCellLeave(index) {
     // TODO: react to mouse leave
+  }
+
+  onNewGameClick() {
+    this.startNewGame();
+  }
+
+  startNewGame() {
+    this.userTeamPositions = new Set();
+    this.aiTeamPositions = new Set();
+    this.userTeam = generateTeam([Bowman, Swordsman], 1, 2);
+    this.aiTeam = generateTeam(this.aiTypes, 1, 2);
+    this.userTeam.forEach(member => {
+      this.userTeamPositions.add(new PositionedCharacter(member, characterPosition().next().value));
+    });
+    this.aiTeam.forEach(member => {
+      this.aiTeamPositions.add(new PositionedCharacter(member, characterPosition('enemy').next().value));
+    });
+    this.gamePlay.redrawPositions([...this.userTeamPositions, ...this.aiTeamPositions]);
+
+    console.log(this.userTeamPositions);
   }
 }
